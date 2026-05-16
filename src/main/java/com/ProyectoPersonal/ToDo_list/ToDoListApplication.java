0e54a8eee1df97c53432f5304ea44456069bd0fd
Sharon Rodriguez <sharo2599@gmail.com>
2026-05-16 16:41:51 -0500

    feat: Iniciar proyecto ToDo-list con Spring boot

---
+++ src/main/java/com/ProyectoPersonal/ToDo_list/ToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoPersonal.ToDo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProyectoPersonal/ToDo_list/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoPersonal.ToDo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
