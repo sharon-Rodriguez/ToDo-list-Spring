a3bddb3bd0f9b066d707d226a66c744e45d722dc
Sharon Rodriguez <sharo2599@gmail.com>
2026-05-17 20:15:19 -0500

    feat: crear controller TareaController

---
+++ src/main/java/com/ProyectoPersonal/ToDo_list/controller/TareaController.java
@@ -0,0 +1,38 @@
+package com.ProyectoPersonal.ToDo_list.controller;
+
+import com.ProyectoPersonal.ToDo_list.model.Tarea;
+import com.ProyectoPersonal.ToDo_list.service.TareaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tareas")
+
+public class TareaController {
+
+    @Autowired
+    private TareaService tareaService;
+
+    @GetMapping
+    public List<Tarea> VerTareas(){
+        return tareaService.VerTareas();
+    }
+
+    @PostMapping
+    public Tarea CrearTarea(@RequestBody Tarea tarea){
+        return tareaService.CrearTarea(tarea);
+    }
+
+    @PutMapping ("/{id}")
+    public Tarea Actualizar(@PathVariable Long id, @RequestBody Tarea tarea){
+        return tareaService.ActualizarTarea(id, tarea);
+    }
+
+    @DeleteMapping ("/{id}")
+    public void Eliminar(@RequestBody Long id){
+        tareaService.EliminarTarea(id);
+    }
+
+}
